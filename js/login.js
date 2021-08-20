0ccc4c991de208b4b749042a1d97f6412cc1f859
macaizaguirre <88292883+macaizaguirre@users.noreply.github.com>
2021-08-19 22:30:17 -0300

    Productos y login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,26 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(){
+document.getElementById("boton").addEventListener("click", function(){
+    let inputcorreo = document.getElementById("inputcorreo");
+    let inputpassword =document.getElementById("inputpassword");
+    let camposcompletos=true;
 
+    if(inputcorreo.value==='') {
+        inputcorreo.classList.add("invalid");
+        camposcompletos=false;
+    }  else{
+        inputcorreo.classList.remove("invalid");
+    }
+
+    if(inputpassword.value===''){
+        inputpassword.classList.add("invalid");
+        camposcompletos=false;
+    }  else{
+        inputpassword.classList.remove("invalid");
+    }
+    if(camposcompletos){
+       window.location='home.html';
+    }
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,44 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+document.addEventListener("DOMContentLoaded", function(e){
 });
+    async function showProductsList(){
+        
+            let promise = await fetch ("https://japdevdep.github.io/ecommerce-api/product/all.json")
+              let data = await promise.json()
+          
+          
+        let htmlProducts = "";
+        for(let index = 0; index < data.length; index++){
+            
+                htmlProducts += `
+                <a href="products.html" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${data[index].imgSrc}
+                            " alt="${data[index].description}
+                            " class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">${data[index].name}
+                                </h4>
+                                <small class="text-muted">${data[index].soldCount}
+                                artículos</small>
+                            </div>
+                            <p class="mb-1">${data[index].description}
+                            </p>
+                            <p> ${data [index].cost} ${data[index].currency}</p>
+                        </div>
+                    </div>
+                </a>
+                `
+            }
+
+
+         let informacion=`${htmlProducts}`
+         let nuevodiv=document.createElement("div");
+         nuevodiv.setAttribute("Id", "productlist");
+         document.body.appendChild(nuevodiv);
+        document.getElementById("productlist").innerHTML = informacion;
+    };
+         showProductsList();

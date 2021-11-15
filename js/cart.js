07b545fb17d13ffb1708e8fac6eb565df8034321
macaizaguirre <88292883+macaizaguirre@users.noreply.github.com>
2021-11-14 22:20:50 -0300

    Carrito con envío

--- js/cart.js
+++ js/cart.js
@@ -25,10 +25,9 @@ document.addEventListener("DOMContentLoaded", function(e){
                  <tr>
                  <td><img src= "${articles.src}"width="100"></td>
                  <td>${articles.name}</td>
-                 <td><input type="number" onchange='multiplicacionSub()' value="${articles.count}" max="" min=1></td>
+                 <td><input type="number" id= "input" onchange='subtotal()' value="${articles.count}"  max="" min=1></td>
                  <td class="precio">${articles.unitCost} ${articles.currency}</td>
                  <td id="sub"></td>
-
                 
                
                `
@@ -44,29 +43,28 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 });
 
-function multiplicacionSub (){
-  let precios = document.getElementsByClassName("precio");
-  let cantidades = document.getElementsByTagName ('input');
-  let multiplicacionSub=0;
 
-   for (let i=0; i< precios.length; i++){
+function subtotal (){
+  let subtotall=0;
+  let precioo = document.getElementsByClassName("precio");
+let cantidades = document.getElementsByTagName ('input');
+   for (let i=0; i< precioo.length; i++){
   
-    multiplicacionSub+= parseFloat(precios[i].innerHTML) * parseFloat(cantidades[i].value);
+    subtotall+= parseFloat(precioo[i].innerHTML) * parseFloat(cantidades[i].value);
 
-    document.getElementById("sub").innerHTML = parseFloat(precios[i].innerHTML) * parseFloat(cantidades[i].value);
 }
 
-document.getElementById('sub').innerHTML=`${(multiplicacionSub)} UYU `;
+document.getElementById('sub').innerHTML=`UYU <span id="subtotalotro">${(subtotall)}</span>`;
+document.getElementById('subtotal').innerHTML=`UYU ${(subtotall)}`;
 }
-
 
 
 document.addEventListener('DOMContentLoaded',()=>{
     
   getJSONData(CART_INFO_URL).then( resultado=>{
       if (resultado.status=="ok"){
-           cartdatos = resultado.data;
-           multiplicacionSub();
+           datosCarrito = resultado.data;
+           subtotal();
               
           
       }
@@ -74,3 +72,89 @@ document.addEventListener('DOMContentLoaded',()=>{
   
 })
 
+
+
+function totalCosto(){
+  let costofinal= parseFloat(document.getElementById("subtotalotro").innerHTML);
+  let costodelenvio = document.getElementById("envio");
+  let preciototalfinal = document.getElementById("total");
+
+
+  let costoEnvio = (Math.round(costofinal * porcentaje * 100) / 100);
+  let totalPrecio = (Math.round(costoEnvio + costofinal));
+
+  costodelenvio.innerHTML = costoEnvio;
+  preciototalfinal.innerHTML =  totalPrecio; 
+}
+
+
+document.getElementById("premium").addEventListener("change", function(){
+  porcentaje = 0.15;
+  totalCosto();
+});
+
+document.getElementById("express").addEventListener("change", function(){
+  porcentaje = 0.07;
+  totalCosto();
+});
+
+document.getElementById("standard").addEventListener("change", function(){
+  porcentaje = 0.05;
+  totalCosto();
+});
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    
+    var forms = document.getElementsByClassName('needs-validation');
+    
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+
+
+
+
+function validacion(){
+  let articulos = document.getElementsByClassName("articlesCount");
+  let cantidadDeArticulos = 0;
+  
+
+  let calle = document.getElementById("validarcalle").value;
+  let esquina = document.getElementById("validaresquina").value;
+  let numero = document.getElementById("validarnumero").value;
+  
+  let tarjeta = document.getElementById("tarjetainfo").value;
+  let codigo = document.getElementById("codigoinfo").value;
+  let vencimiento = document.getElementById("vencimiento").value;
+  
+  let cuenta = document.getElementById("cuentainfo").value;
+  
+  let costo = document.getElementById("envio").innerHTML;
+  
+  for (let i=0; i< articulos.length; i++){
+    
+   cantidadDeArticulos+= parseFloat(articulos[i].value);
+  
+  }
+  if (cantidadDeArticulos != 0  && calle != "" && esquina != "" && numero != ""
+  && tarjeta != "" && codigo != "" && vencimiento != "" && costo != "" || 
+  cantidadDeArticulos != 0  && calle != "" && esquina != "" && cuenta != ""
+  && costo != "" ) {
+    alert("Su compra fue realizada con éxito :)");
+  }
+  else {
+    alert("Su compra no se pudo efecturar,complete todos los campos :/");
+  }
+  
+  }
